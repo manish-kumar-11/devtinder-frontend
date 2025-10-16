8a9b5b3b8d49d0d17c587f50b47a46b6b8c884fe
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-10-16 13:04:40 +0530

    built connection feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./Profile"
 import { Provider } from "react-redux"
 import appStore from "./utils/appStore"
 import Feed from "./Feed"
+import Connections from "./Connections"
 
 
 function App() {
@@ -22,6 +23,8 @@ function App() {
          <Route path="/" element={<Feed />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile/>}/>
+        <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Profile />} />
       </Route>
 
     </Routes>
